07a1419466e73bddf457603bd98e61fdec21e2f9
Surya Jasper <suryajasper2004@gmail.com>
2020-03-24 12:04:59 -0700

    Added socket.io code

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+var gameID = process.env.PORT || 2000;
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/index.html');
 });
-app.use('/client', express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client'));
 
-serv.listen(2000);
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(gameID, function(){
+  console.log('listening on port' + gameID.toString());
+});
