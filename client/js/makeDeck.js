d75f6ae91fd228bde15107e5e9d0121b2cc05f65
Surya Jasper <suryajasper2004@gmail.com>
2020-04-14 23:26:36 -0700

    added options menu

--- client/js/makeDeck.js
+++ client/js/makeDeck.js
@@ -1,3 +1,16 @@
+var categories = [];
+var JSONobj = null;
+
+function downloadObjectAsJson(exportObj, exportName){
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  var downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href",     dataStr);
+  downloadAnchorNode.setAttribute("download", exportName + ".json");
+  document.body.appendChild(downloadAnchorNode); // required for firefox
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
 function createInput(prompt) {
   var newListElement = document.createElement('li');
   var newInput = document.createElement('input');
@@ -9,7 +22,21 @@ function createInput(prompt) {
   return newListElement;
 }
 
+function convertElementsToJSON() {
+  for (var i = 0; i < categories.length; i++) {
+    var childnodes = document.getElementById(categories[i]).children;
+    JSONobj[categories[i]] = [];
+    for (var j = 0; j < childnodes.length; j++) {
+      var inputField = childnodes[j].children[0];
+      JSONobj[categories[i]].push(inputField.value);
+    }
+  }
+  console.log(JSONobj);
+}
+
 function makeEverything(questions) {
+  JSONobj = questions;
+  categories = Object.keys(questions);
   for (var i = 0; i < Object.keys(questions).length; i++) (function(i) {
     var category = Object.keys(questions)[i];
     console.log('cat ' + category);
@@ -19,6 +46,8 @@ function makeEverything(questions) {
       divToAppendTo.appendChild(createInput(questions[category][j]));
     }
     document.getElementById('button' + category).onclick = function() {
+      convertElementsToJSON();
+      window.localStorage.setItem('questions', JSON.stringify(JSONobj));
       divToAppendTo.appendChild(createInput(null));
     }
   })(i);
@@ -38,10 +67,20 @@ function loadJSON(filename, callback) {
 }
 
 if (window.localStorage.getItem('questions') != null) {
-  makeEverything(window.localStorage.getItem('questions'));
+  makeEverything(JSON.parse(window.localStorage.getItem('questions')));
+  document.getElementById('download').onclick = function() {
+    downloadObjectAsJson(JSON.parse(window.localStorage.getItem('questions')), 'genprompts');
+  }
 } else {
   loadJSON('questions.json', function(json) {
-    console.log('we here');
     makeEverything(JSON.parse(json));
   });
+  document.getElementById('download').onclick = function() {
+    convertElementsToJSON();
+    downloadObjectAsJson(JSONobj, 'genprompts');
+  }
+}
+document.getElementById('save').onclick = function() {
+  convertElementsToJSON();
+  window.localStorage.setItem('questions', JSON.stringify(JSONobj));
 }
